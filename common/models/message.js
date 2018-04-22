3b5833a66344ac1c11284d17fd420db7a6f03679
wkwksama <mailpakgasing@gmail.com>
2018-04-22 08:58:14 +0700

    message for subscriber

--- common/models/message.js
+++ common/models/message.js
@@ -1,5 +1,20 @@
 'use strict';
 
 module.exports = function(Message) {
-
+  Message.afterRemote('create', function(context, user, next) {
+    if (context.args.data.isSubscribe) {
+      let app = Message.app;
+      let Subscriber = app.models.Subscriber;
+      Subscriber.create({
+        fullName: context.args.data.fullName,
+        nickName: context.args.data.nickName,
+        email: context.args.data.email
+      }).then(result => {
+        console.log('berhasil create subscriber, ', result);
+      }).catch(error => {
+        console.log('error create subscriber, ', error);
+      });
+    }
+    next();
+  });
 };
