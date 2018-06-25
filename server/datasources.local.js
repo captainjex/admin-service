a76e16937719f5dd9b75e9d7f9ab6efc694908d5
wkwksama <mailpakgasing@gmail.com>
2018-06-25 15:23:06 +0700

    dotenv

---
+++ server/datasources.local.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mongoDB: {
+    connector: 'mongodb',
+    hostname: process.env.MONGO_HOST || 'localhost',
+    port: process.env.MONGO_PORT || 27017,
+    user: process.env.MONGO_USER || '',
+    password: process.env.MONGO_PASSWORD || '',
+    database: process.env.MONGO_DATABASE || 'example'
+  }
+};
